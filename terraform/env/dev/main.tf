ff75986a95c1005f1c2ea1804afb3feda16a6f40
santhanakrishnanbtech <santhanakrishnanbtech@gmail.com>
2020-10-09 13:52:29 +0530

    sk: final review

--- terraform/env/dev/main.tf
+++ terraform/env/dev/main.tf
@@ -1,20 +1,15 @@
 terraform {
-  required_version = "~> 0.12.0"
+  required_version                    = "~> 0.12.0"
 }
 
 provider "aws" {
-  region = "ap-south-1"
-  profile = "dev"
+  region                              = "ap-south-1"
+  profile                             = "dev"
 }
 
-//resource "aws_default_vpc" "default" {
-//  tags = {
-//    Name = "Default VPC"
-//  }
-//}
 #--------------------------------------------------------------------[ VPC ]
 module "vpc" {
-  source = "../../modules/vpc"
+  source                              = "../../modules/vpc"
   map_public_ip_on_launch             = "true"
   vpc-cidr                            = "10.0.0.0/16"
   vpc-public-subnet-cidr              = "10.0.1.0/24"
@@ -23,90 +18,84 @@ module "vpc" {
 #--------------------------------------------------------------------[ SG NO REF ]
 module "ci_sg" {
   source                              = "../../modules/sg1"
-  security_group_name        = "ci"
+  security_group_name                 = "ci"
   tcp_ports                           = "22,80,81"
   cidr                                = ["0.0.0.0/0"]
   vpc_id                              = module.vpc.vpc-id
-//  vpc_id                              = aws_default_vpc.default.id
 }
 #--------------------------------------------------------------------[ SG REF ]
 module "web_sg" {
-  source                      = "../../modules/sg2-ref"
-  security_group_name        = "web"
-  tcp_ports                   = "22,80"
-  ref_security_groups_ids     = module.ci_sg.aws_security_group_default
-    vpc_id                      = module.vpc.vpc-id
-//  vpc_id                      = aws_default_vpc.default.id
+  source                              = "../../modules/sg2-ref"
+  security_group_name                 = "web"
+  tcp_ports                           = "22,80"
+  ref_security_groups_ids             = module.ci_sg.aws_security_group_default
+    vpc_id                            = module.vpc.vpc-id
 }
 #--------------------------------------------------------------------[ SG REF ]
 module "db_sg" {
-  source = "../../modules/sg3-db"
-  security_group_name        = "db"
-//  vpc_id                      = aws_default_vpc.default.id
-  vpc_id                     =  module.vpc.vpc-id
+  source                              = "../../modules/sg3-db"
+  security_group_name                 = "db"
+  vpc_id                              =  module.vpc.vpc-id
 
   # Rule-1
-  ingress-rule-1-from-port        = 22
-  ingress-rule-1-to-port          = 22
-  ingress-rule-1-protocol         = "tcp"
-  ingress-rule-1-sg          = [module.ci_sg.aws_security_group_default]
-  ingress-rule-1-description      = "Bastion Conn"
+  ingress-rule-1-from-port            = 22
+  ingress-rule-1-to-port              = 22
+  ingress-rule-1-protocol             = "tcp"
+  ingress-rule-1-sg                   = [module.ci_sg.aws_security_group_default]
+  ingress-rule-1-description          = "Bastion Conn"
 
   # Rule-2
-  ingress-rule-2-from-port          = 3306
-  ingress-rule-2-to-port            = 3306
-  ingress-rule-2-protocol           = "tcp"
-  ingress-rule-2-security-groups    = [module.web_sg.aws_security_group_default]
-  ingress-rule-2-description        = "Web Conn"
+  ingress-rule-2-from-port            = 3306
+  ingress-rule-2-to-port              = 3306
+  ingress-rule-2-protocol             = "tcp"
+  ingress-rule-2-security-groups      = [module.web_sg.aws_security_group_default]
+  ingress-rule-2-description          = "Web Conn"
 
   # Rule-6
-  ingress-rule-3-from-port        = -1
-  ingress-rule-3-to-port          = -1
-  ingress-rule-3-protocol         = "icmp"
-  ingress-rule-3-cidrs  = ["0.0.0.0/0"]
-  ingress-rule-3-description      = "Icmp Allowed"
+  ingress-rule-3-from-port            = -1
+  ingress-rule-3-to-port              = -1
+  ingress-rule-3-protocol             = "icmp"
+  ingress-rule-3-cidrs                = ["0.0.0.0/0"]
+  ingress-rule-3-description          = "Icmp Allowed"
 }
 #--------------------------------------------------------------------[ CONCOURSE/BASTION/NGINX ]
 module "concourse" {
-  source = "../../modules/ec2"
-  instance_name = "dev-mediawiki-ci"
-  ami = "ami-0e306788ff2473ccb"
-  private_ips = "10.0.1.10"
-  instance_count = "1"
-  instance_type = "t2.micro"
-  key_name = "PROD002"
-//  subnet_id = "subnet-aa4749c2"
-  subnet_id = module.vpc.public-subnet
-  vpc_security_group_ids = [module.ci_sg.aws_security_group_default]
-  associate_public_ip_address   = "true"
+  source                              = "../../modules/ec2"
+  instance_name                       = "dev-mediawiki-ci"
+  ami                                 = "ami-0e306788ff2473ccb"
+  private_ips                         = "10.0.1.10"
+  instance_count                      = "1"
+  instance_type                       = "t2.micro"
+  key_name                            = "PROD002"
+  subnet_id                           = module.vpc.public-subnet
+  vpc_security_group_ids              = [module.ci_sg.aws_security_group_default]
+  associate_public_ip_address         = "true"
 }
 #--------------------------------------------------------------------[ WEB ]
 module "web" {
-  source = "../../modules/ec2-other"
-  instance_name = "dev-mediawiki-web"
-  ami = "ami-0e306788ff2473ccb"
-  private_ips = "10.0.2.11"
-  instance_count = "1"
-  instance_type = "t2.micro"
-  key_name = "PROD002"
-//  subnet_id = "subnet-aa4749c2"
-  subnet_id = module.vpc.private-subnet
-  vpc_security_group_ids = [module.web_sg.aws_security_group_default]
-  associate_public_ip_address   = "false"
+  source                              = "../../modules/ec2-other"
+  instance_name                       = "dev-mediawiki-web"
+  ami                                 = "ami-0e306788ff2473ccb"
+  private_ips                         = "10.0.2.11"
+  instance_count                      = "1"
+  instance_type                       = "t2.micro"
+  key_name                            = "PROD002"
+  subnet_id                           = module.vpc.private-subnet
+  vpc_security_group_ids              = [module.web_sg.aws_security_group_default]
+  associate_public_ip_address         = "false"
 }
 #--------------------------------------------------------------------[ DB ]
 module "db" {
-  source = "../../modules/ec2-other"
-  instance_name = "dev-mediawiki-db"
-  ami = "ami-0e306788ff2473ccb"
-  private_ips = "10.0.2.12"
-  instance_count = "1"
-  instance_type = "t2.micro"
-  key_name = "PROD002"
-//  subnet_id = "subnet-aa4749c2"
-  subnet_id = module.vpc.private-subnet
-  vpc_security_group_ids = [module.db_sg.aws_security_group]
-  associate_public_ip_address   = "false"
+  source                              = "../../modules/ec2-other"
+  instance_name                       = "dev-mediawiki-db"
+  ami                                 = "ami-0e306788ff2473ccb"
+  private_ips                         = "10.0.2.12"
+  instance_count                      = "1"
+  instance_type                       = "t2.micro"
+  key_name                            = "PROD002"
+  subnet_id                           = module.vpc.private-subnet
+  vpc_security_group_ids              = [module.db_sg.aws_security_group]
+  associate_public_ip_address         = "false"
 }
 
 
--- terraform/env/dev/outputs.tf
+++ terraform/env/dev/outputs.tf
@@ -5,9 +5,9 @@ output "vpc-id" {
 output "concourse_ip" {
   value = module.concourse.public_ip
 }
-output "web_ip" {
-  value = module.web.public_ip
-}
-output "db_ip" {
-  value = module.db.public_ip
-}
+//output "web_ip" {
+//  value = module.web.public_ip
+//}
+//output "db_ip" {
+//  value = module.db.public_ip
+//}
